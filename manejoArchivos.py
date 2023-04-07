0527de53578f2a7b4671beafb6cdaaa4f996bc8a
elizabeth selva <elizabeth.selva@uned.cr>
2023-04-06 19:06:22 -0600

    "Se añade control de excepciones en la clase para el manejo de archivos y se agrega una carpeta para almacenar los archivos"

--- manejoArchivos.py
+++ manejoArchivos.py
@@ -4,12 +4,32 @@ import os
 
 class ManejoArchivo:
 
+    def __init__(self):
+
+        # Se obtiene el directorio actual
+        directorioActual = os.getcwd()
+
+        # Se define el nombre de la carpeta donde estarán los archivos
+        self.carpetaArchivos = directorioActual + "/" + "directorioArchivos"
+
+        # Se crea la carpeta para almacenar los archivos
+        # y se controla el error en caso tal de que se intente crear la carpeta
+        # sabiendo que ya existe en el sistema
+        try:
+            os.mkdir(self.carpetaArchivos)
+        except OSError as error:
+            print("Como la carpeta de archivos ya existe no se creará de nuevo")
+
     # Esta función permite crear el archivo a partir del nombre ingresado por el usuario
     # en la variable nombreArchivo,
     # no se retorna información.
     def CrearArchivo(self, nombreArchivo):
+
+        # Se añade a la ruta de la carpeta el nombre del archivo donde se almacenará el inventario de libros
+        archivoInventario = self.carpetaArchivos + "/" + nombreArchivo
+
         # Se crea el archivo
-        f = open(nombreArchivo, "w")
+        f = open(archivoInventario, "w")
 
         # Se cierra el archivo
         f.close()
@@ -18,8 +38,12 @@ class ManejoArchivo:
     # se ingresa el nombre el archivo y el texto a ser escrito en el archivo,
     # no se retorna
     def AnadirContenidoAlArchivo(self, nombreArchivo, texto):
+
+        # Variable con la ruta completa del archivo de inventario
+        archivoInventario = self.carpetaArchivos + "/" + nombreArchivo
+
         # Se abre el archivo
-        f = open(nombreArchivo, "a")
+        f = open(archivoInventario, "a")
 
         # Se escribe en el archivo el texto ingresado como parámetro
         f.write(texto)
@@ -34,8 +58,12 @@ class ManejoArchivo:
     # ingresado por el usuario en la variable nombreArchivo,
     # no se retorna información.
     def LeerArchivo(self, nombreArchivo):
+
+        # Variable con la ruta completa del archivo de inventario
+        archivoInventario = self.carpetaArchivos + "/" + nombreArchivo
+
         # Se abre el archivo
-        f = open(nombreArchivo, "r")
+        f = open(archivoInventario, "r")
 
         # Se lee el archivo y se imprime en consola el contenido
         print(f.read())
@@ -47,5 +75,13 @@ class ManejoArchivo:
     # se ingresa el nombre el archivo a ser eliminado en los parámetros,
     # no se retorna información
     def EliminarArchivo(self, nombreArchivo):
-        # Se elimina el archivo
-        os.remove(nombreArchivo)
+
+        # Variable con la ruta completa del archivo de inventario
+        archivoInventario = self.carpetaArchivos + "/" + nombreArchivo
+
+        # Se intenta eliminar el archivo, si no existe se controla el error generado por
+        # el sistema al no encontrar el archivo
+        try:
+            os.remove(archivoInventario)
+        except OSError as error:
+            print("Como el archivo no existe no se borrará y se controla el error")
